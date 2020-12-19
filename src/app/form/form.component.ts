ef309bd21273d7f909c5b41d88e0fee8fbbc63ab
Jocelyne Piguave <josychirlider@hotmail.com>
2020-12-19 06:41:54 -0500

    the form gets the data successfully

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Plate } from 'src/app/models/plateForm.model';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
+  plateAction(formData: NgForm): void {
+    if (formData.valid) {
+      const plateForm: Plate = {
+        plate: formData.value.plate,
+        date: formData.value.date,
+        hour: formData.value.hour
+      };
+      console.log(plateForm);
+    }
   }
 
 }
